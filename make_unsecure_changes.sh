e363e57c5c05c1f477aba2a2d56977ade2d1092d
Max Fröhlich <maxbruchmann@gmail.com>
2022-07-13 13:25:22 +0200

    fix append

--- make_unsecure_changes.sh
+++ make_unsecure_changes.sh
@@ -6,10 +6,10 @@
 # the EXAMPLE and PART vars are there to obfuscate the credentials, otherwise trivy would complain about this file. 
 
 EXAMPLE="EXAMPLE"
-echo "ENV AWS_ACCESS_KEY_ID=AKIAIOSFODNN7${EXAMPLE}" > Dockerfile
+echo "ENV AWS_ACCESS_KEY_ID=AKIAIOSFODNN7${EXAMPLE}" >> Dockerfile
 
 PART1="wJalrXUtnFEMI"
 PART2="K7MDENG"
 PART3="bPxRfiCY${EXAMPLE}KEY"
 
-echo "ENV AWS_SECRET_ACCESS_KEY=${PART1}/${PART2}/${PART3}" > Dockerfile
+echo "ENV AWS_SECRET_ACCESS_KEY=${PART1}/${PART2}/${PART3}" >> Dockerfile
